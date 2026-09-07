166796f877c16e28d5fb6bafd3c8f5b593e0593c
Smartphune <151577244+Smartphune@users.noreply.github.com>
2026-09-08 00:41:54 +0200

    Update title of Research Question 3 page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 overview = st.Page("overview.py", title="Overview")
 q1 = st.Page("research_questions/question_1.py", title="Research Question 1")
 q2 = st.Page("research_questions/question_2.py", title="Research Question 2")
-q3 = st.Page("research_questions/question_3.py", title="Research Question 3 - Introduction")
+q3 = st.Page("research_questions/question_3.py", title="Research Question 3")
 q4 = st.Page("research_questions/question_4.py", title="Research Question 4")
 q5 = st.Page("research_questions/question_5.py", title="Research Question 5")
 q6 = st.Page("research_questions/question_6.py", title="Research Question 6")
